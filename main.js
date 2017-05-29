b01db63f209da1644949aa17b56808cee5409d79
Muhammad Ilham <ilham@getcraft.asia>
2017-05-29 13:38:50 +0700

    use superagent instead request package

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-let cheerio = require('cheerio')
-
-const request = require('request');
-
+const cheerio = require('cheerio')
+const request = require('superagent');
 
 function converter(options) {
-  const from = options.from
-  const to = options.to
-  const amount = options.amount
   return new Promise(function(resolve, reject) {
-    const url = 'https://www.google.com/finance/converter?a='+ amount +'&from='+ from +'&to=' + to
-    request(url, function (error, response, body) {
-      const $ = cheerio.load(body, {normalizeWhitespace: true})
-      const fromResult = $( "select[name='from']" ).val()
-      const toResult = $( "select[name='to']" ).val()
-      const amountResult = $( "input[name='a']" ).val()
-      const converted = $('#currency_converter_result .bld').text()
-      if (!error && response.statusCode == 200) {
-        resolve({
-          from: fromResult,
-          to: toResult,
-          amount: amount,
-          converted: parseFloat(converted),
-          url: url
-        })
-      } else {
-        reject(error);
-      }
-    })
+    const source = 'https://www.google.com/finance/converter'
+    const queryString = {
+      a: options.amount,
+      from: options.from,
+      to: options.to
+    }
+    request
+      .get(source)
+      .query(queryString)
+      .end(function(error, response){
+        const $ = cheerio.load(response.text, {normalizeWhitespace: true})
+        const fromResult = $( "select[name='from']" ).val()
+        const toResult = $( "select[name='to']" ).val()
+        const amountResult = $( "input[name='a']" ).val()
+        const converted = $('#currency_converter_result .bld').text()
+
+        if (!error && response.statusCode == 200) {
+          resolve({
+            from: fromResult,
+            to: toResult,
+            amount: amountResult,
+            converted: parseFloat(converted),
+            url: source + response.req.path
+          })
+        } else {
+          reject(error);
+        }
+      })
   })
 }
 
